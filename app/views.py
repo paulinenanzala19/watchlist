1eaad4cf8a5a2b447eba658cacc8421b01abd356
paulinenanzala19 <pauline.wafula@student.moringaschool.c0m>
2022-04-28 00:26:49 +0300

    adding a css file in the static folder

--- app/views.py
+++ app/views.py
@@ -1,6 +1,6 @@
 from flask import render_template
 from app import app
-from .request import get_movies
+from .request import get_movies,get_movie
 
 
 # Views
@@ -19,10 +19,21 @@ def index():
     title = 'Home - Welcome to The best Movie Review Website Online'
     return render_template('index.html', title = title, popular=popular_movies, upcomimg=upcoming_movie, now_showing=now_showing_movie)
 
-@app.route('/movie/<int:movie_id>')
-def movie(movie_id):
+# @app.route('/movie/<int:movie_id>')
+# def movie(movie_id):
+#     '''
+#     View root page function that returns the index page and its data
+#     '''
+#     # title = f"Hello"
+#     return render_template('movie.html',id = movie_id)
+
+@app.route('/movie/<int:id>')
+def movie(id):
+
     '''
-    View root page function that returns the index page and its data
+    View movie page function that returns the movie details page and its data
     '''
-    # title = f"Hello"
-    return render_template('movie.html',id = movie_id)
+    movie = get_movie(id)
+    title = f'{movie.title}'
+
+    return render_template('movie.html',title = title,movie = movie)
